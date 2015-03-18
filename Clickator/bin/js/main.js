ca988bcce605d9b53ef677bbcde07e9737183ef0
Aurélien COLOMBET <colombet.aurelien@etu.unice.fr>
2015-03-18 10:03:55 +0100

    modification du jeu + try du cursor a faire

--- Clickator/bin/js/main.js
+++ Clickator/bin/js/main.js
@@ -1,7 +1,18 @@
 jQuery.noConflict();
 
+var canvas = document.getElementById('gameArea');
+var ctx = canvas.getContext('2d');
+
+var canvasOffset;
+var canvasX;
+var canvasY;
+var imageData;
+var pixel;
+var pixelColor;
+
 //alert("ici");
 //ICI TEST CANVAS
+/*
 jQuery('#gameArea').drawArc({
     //draggable: true, //Pour dire si l'élément est drag and droppable
     layer:true,
@@ -10,9 +21,29 @@ jQuery('#gameArea').drawArc({
     fillStyle: 'rgb(204, 51, 51)',
     radius: 25,
     width: 0, height: 0,
-    click: function(layer){jQuery("#gameArea").removeLayer(layer).drawLayers();}
+    click: function(layer){
+        jQuery("#gameArea").removeLayer(layer).drawLayers();
+    }
 });
+*/
 
+for(var i=0;i<10;i++){
+    var xTEMP=Math.floor((Math.random() * 750) + 1);
+    var yTEMP=Math.floor((Math.random() * 350) + 1);
+    jQuery('#gameArea').drawImage({
+	layer:true,
+	source: 'Image/mouton.png',
+	scale: 0.35,
+	x:xTEMP  , y: yTEMP,
+	click: function(layer){
+	    var tmp = getPixelColor();
+	    if (!(tmp[0]==0 && tmp[1]==0 && tmp[2]==0 && tmp[3]==0)) {
+		jQuery("#gameArea").removeLayer(layer).drawLayers();
+	    }
+	}
+    });
+}
+/*
 for(var i=0;i<10;i++){
     var xTEMP=Math.floor((Math.random() * 750) + 1);
     var yTEMP=Math.floor((Math.random() * 350) + 1);
@@ -23,11 +54,12 @@ for(var i=0;i<10;i++){
 	fillStyle: 'rgb(204, 51, 51)',
 	radius: 25,
 	width: 0, height: 0,
-	click: function(layer){jQuery("#gameArea").removeLayer(layer).drawLayers();}
+	click: function(layer){
+	    jQuery("#gameArea").removeLayer(layer).drawLayers();
+	}
     });
 }
-//jQuery(tmp).css({"display":"none"});
-//alert(tmp);
+*/
     
     /*Une animation de test
     jQuery('#gameArea').animateLayer('myBox', {
@@ -43,6 +75,19 @@ for(var i=0;i<10;i++){
       });
       */
 /*TODO faire le main du jeu*/
+
+jQuery(document).mousemove(function(e){
+    canvasOffset = jQuery("#gameArea").offset();
+    canvasX = Math.floor(e.pageX - canvasOffset.left);
+    canvasY = Math.floor(e.pageY - canvasOffset.top);
+});
+
+function getPixelColor(){
+    imageData = ctx.getImageData(canvasX, canvasY, 1, 1);
+    pixel = imageData.data;
+    //pixelColor = "rgba("+pixel[0]+", "+pixel[1]+", "+pixel[2]+", "+pixel[3]+")";
+    return pixel;
+}
 
 /*
 var Main = function () {
