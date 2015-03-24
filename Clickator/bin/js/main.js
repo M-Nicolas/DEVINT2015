e62c71313c9e759030c9ad1ba89426435930977f
Aurélien COLOMBET <colombet.aurelien@etu.unice.fr>
2015-03-24 15:47:38 +0100

    Mise en place de la loop game (commente pour nasseredine)

--- Clickator/bin/js/main.js
+++ Clickator/bin/js/main.js
@@ -9,23 +9,7 @@ var canvasY;
 var imageData;
 var pixel;
 var pixelColor;
-
-for(var i=0;i<10;i++){
-    var xTEMP=Math.floor((Math.random() * 750) + 1);
-    var yTEMP=Math.floor((Math.random() * 350) + 1);
-    jQuery('#gameArea').drawImage({
-	layer:true,
-	source: 'Image/mouton.png',
-	scale: 0.35,
-	x:xTEMP  , y: yTEMP,
-	click: function(layer){
-	    var tmp = getPixelColor();
-	    if (!(tmp[0]==0 && tmp[1]==0 && tmp[2]==0 && tmp[3]==0)) {
-		jQuery("#gameArea").removeLayer(layer).drawLayers();
-	    }
-	}
-    });
-}   
+   
     /*Une animation de test
     jQuery('#gameArea').animateLayer('myBox', {
 	x: 150, y: 150,
@@ -51,4 +35,43 @@ function getPixelColor(){
     pixel = imageData.data;
     //pixelColor = "rgba("+pixel[0]+", "+pixel[1]+", "+pixel[2]+", "+pixel[3]+")";
     return pixel;
+}
+
+
+//setInterval(onTimerTick, /*33*/1000); // 33 milliseconds = ~ 30 frames per sec (loop game)
+
+var numMouton = 0;
+
+function onTimerTick() {
+    numMouton++;
+    //for(var i=0;i<10;i++){
+	var xTEMP=Math.floor((Math.random() * 750) + 1);
+	var yTEMP=Math.floor((Math.random() * 350) + 1);
+	jQuery('#gameArea').drawImage({
+	    layer:true,
+	    name:'mout'+numMouton,
+	    source: 'Image/mouton.png',
+	    scale: 0.35,
+	    x:xTEMP  , y: yTEMP,
+	    click: function(layer){
+		var tmp = getPixelColor();
+		if (!(tmp[0]==0 && tmp[1]==0 && tmp[2]==0 && tmp[3]==0)) {
+		    jQuery("#gameArea").removeLayer(layer).drawLayers();
+		}
+	    }
+	});
+	
+	//jQuery('#gameArea').animateLayer('mout'+numMouton,)
+	var tmpLay = jQuery("#gameArea").getLayer('mout'+numMouton);
+	var tmpWidthMout = tmpLay.width;
+	var tmpHeightMout = tmpLay.height;
+	tmpLay.width=0;//350
+	tmpLay.height=0;//350
+	//alert(tmpLay);
+	//tmpLay.animate({left: '250px'});
+	jQuery('#gameArea').animateLayer('mout'+numMouton, {
+	    height: '+='+tmpHeightMout,
+	    width: '+='+tmpWidthMout
+	  });
+    //}
 }
